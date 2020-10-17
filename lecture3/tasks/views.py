29d7fe588dca44545e3f23912192429209f35446
Xylios13 <gbarreto1992@gmail.com>
2020-10-17 15:53:14 +0100

    Implement add task behaviour

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -7,7 +7,8 @@ tasks = ["foo", "bar", "baz"]
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
-    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
+    # Do not care about priority for the example
+    # priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 def index(request):
     return render(request, "tasks/index.html", {
@@ -15,6 +16,16 @@ def index(request):
     })
 
 def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+
     return render(request, "tasks/add.html", {
         "form": NewTaskForm()
     })
