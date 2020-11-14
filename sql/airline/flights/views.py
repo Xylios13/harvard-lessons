d4c92ebe5dc79bddd324cdbc86424e8866bf47b6
Xylios13 <gbarreto1992@gmail.com>
2020-11-14 11:40:54 +0000

    Flesh out flights application

--- sql/airline/flights/views.py
+++ sql/airline/flights/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from .models import Flight
+
 # Create your views here.
+def index(request):
+    return render(request, "flights/index.html", {
+        "flights": Flight.objects.all()
+    })
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(id=flight_id) # or pk=flight_id (stands for primary key)
+    return render(request, "flights/flight.html", {
+        "flight": flight
+    })
